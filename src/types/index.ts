55753e5d1c6dbfdbdf46c605952e9e267a5a40e9
wkl--007 <499657357@qq.com>
2019-05-10 22:26:23 +0800

    feat: 创建axios入口文件，发送请求，demo编写

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+//联合类型
+export type Method =
+  | 'get'
+  | 'Get'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
