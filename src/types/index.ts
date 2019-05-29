1a07fb58ed80dca253a946f0f47a6ead81e250c7
wkl007 <499657357@qq.com>
2019-05-29 16:35:22 +0800

    feat: 扩展axios.create静态方法

--- src/types/index.ts
+++ src/types/index.ts
@@ -95,3 +95,7 @@ export interface RejectedFn {
 export interface AxiosTransformer {
   (data: any, headers?: any): any
 }
+
+export interface AxiosStatic extends AxiosInstance {
+  create(config?: AxiosRequestConfig): AxiosInstance
+}
